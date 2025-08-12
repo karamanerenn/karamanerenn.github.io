adfbab5e53160bb84a093acf371efab73274970c
karamanerenn <karamanerenn@gmail.com>
2025-08-12 17:57:24 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// Yıl
-document.getElementById('year') && (document.getElementById('year').textContent = new Date().getFullYear());
-
-// Mobil menü
-const menuBtn = document.getElementById('menuBtn');
-const nav = document.getElementById('nav');
-if (menuBtn && nav){
-  menuBtn.addEventListener('click', () => nav.classList.toggle('open'));
-}
